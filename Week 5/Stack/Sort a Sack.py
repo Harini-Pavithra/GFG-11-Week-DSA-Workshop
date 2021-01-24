02126c6ed9cccb5d6aa61d2916d363bc9f050ef8
Harini Pavithra <harinipavithra0998@gmail.com>
2021-01-24 15:24:28 +0530

    Create Sort a Sack.py

---
+++ Week 5/Stack/Sort a Sack.py
@@ -0,0 +1,52 @@
+Sort a stack 
+
+Given a stack, the task is to sort it such that the top of the stack has the greatest element.
+
+Example 1:
+
+Input:
+Stack: 3 2 1
+Output: 3 2 1
+Example 2:
+
+Input:
+Stack: 11 2 32 3 41
+Output: 41 32 11 3 2
+Your Task: 
+You don't have to read input or print anything. Your task is to complete the function sort() which sorts the elements present in the given stack. (The sorted stack is printed by the driver's code by popping the elements of the stack.)
+
+Expected Time Complexity : O(N*N)
+Expected Auixilliary Space : O(N) recursive.
+
+Constraints:
+1<=N<=100
+
+Note:The Input/Ouput format and Example given are used for system's internal purpose, and should be used by a user for Expected Output only. As it is a function problem, hence a user should not read any input from stdin/console. The task is to complete the function specified, and not to write the full code.
+
+Solution:
+
+# your task is to complete this function
+# function sort the stack such that top element is max
+# funciton should return nothing
+# s is a stack
+def sorted(s):
+    # Code here
+    return s.sort(reverse = True)
+
+
+
+
+#{ 
+#  Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        sorted(arr)
+        for e in range(len(arr)):
+            print(arr.pop(0), end=" ")
+        print()
+# contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
